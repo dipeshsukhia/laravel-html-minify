d09b959e17eb8f78b29fe9914dfdbdf17556e277
Dipesh Sukhia <dipesh.sukhia@gmail.com>
2023-09-07 08:46:22 +0530

    for production error page apply minify

--- src/Middleware/LaravelMinifyHtml.php
+++ src/Middleware/LaravelMinifyHtml.php
@@ -3,6 +3,7 @@ namespace DipeshSukhia\LaravelHtmlMinify\Middleware;
 
 use Closure;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use DipeshSukhia\LaravelHtmlMinify\LaravelHtmlMinifyFacade;
 
@@ -16,8 +17,8 @@ class LaravelMinifyHtml
             Config::get('htmlminify.default') &&
             $this->isResponseObject($response) &&
             $this->isHtmlResponse($response) &&
-            !$this->isServerError($response) &&
-            !$this->isRouteExclude($request)            
+            !$this->isRouteExclude($request) &&
+            ( $response->isSuccessful() || App::isProduction() )
         ) {
             $response->setContent(LaravelHtmlMinifyFacade::htmlMinify($response->getContent()));
         }
@@ -37,22 +38,5 @@ class LaravelMinifyHtml
     protected function isRouteExclude($request)
     {
         return $request->route() && in_array($request->route()->getName(),config('htmlminify.exclude_route', []));
-    }
-
-    protected function isServerError(Response $response)
-    {
-        return in_array($response->getStatusCode(), [
-            Response::HTTP_INTERNAL_SERVER_ERROR,
-            Response::HTTP_NOT_IMPLEMENTED,
-            Response::HTTP_BAD_GATEWAY,
-            Response::HTTP_SERVICE_UNAVAILABLE,
-            Response::HTTP_GATEWAY_TIMEOUT,
-            Response::HTTP_VERSION_NOT_SUPPORTED,
-            Response::HTTP_VARIANT_ALSO_NEGOTIATES_EXPERIMENTAL,
-            Response::HTTP_INSUFFICIENT_STORAGE,
-            Response::HTTP_LOOP_DETECTED,
-            Response::HTTP_NOT_EXTENDED,
-            Response::HTTP_NETWORK_AUTHENTICATION_REQUIRED,
-        ]);
     }
 }
